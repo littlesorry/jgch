5aa84fa48ff090e17b06a429dc8cb3a37eb24ed4
littlesorry <chaumet.g@gmail.com>
2014-11-09 15:26:22 +0800

    fix code syntax issue

--- sql/create_index.js
+++ sql/create_index.js
@@ -1,2 +1,2 @@
-db.user.ensureIndex( { "openId": 1 }, { unique: true, dropDups: true } );
-db.refer.ensureIndex( { "sourceOpenId": 1, "targetOpenId": 1 }, { unique: true, dropDups: true } );
+db.users.ensureIndex( { "openId": 1 }, { unique: true, dropDups: true } );
+db.refers.ensureIndex( { "sourceOpenId": 1, "targetOpenId": 1 }, { unique: true, dropDups: true } );
